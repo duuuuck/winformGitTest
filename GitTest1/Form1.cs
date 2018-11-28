7d1f382b5b59a89b4b433ef2ceaa140c322b1113
kovv07 <kovv07@gmail.com>
2018-11-28 12:00:43 +0900

    체크박스 , 이벤트 추가

--- GitTest1/Form1.cs
+++ GitTest1/Form1.cs
@@ -31,6 +31,12 @@ namespace GitTest1
         {
 
             this.label1.Text = "Test3";
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("cbox");
         }
     }
 }
